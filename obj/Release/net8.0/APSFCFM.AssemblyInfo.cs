7a2bd01a9bf35ce92de7a4b5e97664656d99e057
Marco Martinez <TheMyak@hotmail.com>
2024-11-13 00:02:11 -0600

    Test v1

--- obj/Release/net8.0/APSFCFM.AssemblyInfo.cs
+++ obj/Release/net8.0/APSFCFM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APSFCFM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51f5a558e8fd700809a3c3f0f665061679f0d645")]
 [assembly: System.Reflection.AssemblyProductAttribute("APSFCFM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APSFCFM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
